2d7a8f14a1834f8e999a999300197f80187be021
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-10-30 15:57:58 +0900

    [Fix] BaseEntity 유연성을 위해 구분 - DateAudit을 만들어서 사용한 뒤, 삭제하는 entity들이 필요한 필드만 사용할 수 있게 수정 - BaseEntity는 deleted를 저장해서 개념에 맞도록 설정

--- src/main/java/com/zts/delivery/global/persistence/common/BaseEntity.java
+++ src/main/java/com/zts/delivery/global/persistence/common/BaseEntity.java
@@ -1,43 +1,43 @@
-package com.zts.delivery.common.entity;
+package com.zts.delivery.global.persistence.common;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+public abstract class BaseEntity extends DateAudit {
 
     @CreatedBy
-    @Column(updatable = false)
+    @Column(updatable = false, name = "created_by", length = 50)
     private String createdBy;
 
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
-
     @LastModifiedBy
+    @Column(name = "update_by", length = 50)
     private String updatedBy;
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    @Column(name = "deleted_by")
+    @Column(name = "deleted_by", length = 50)
     private String deletedBy;
+
+    @Column(name = "etc", columnDefinition = "TEXT")
+    private String etc;
 
     public void markAsDeleted(String deleter) {
         this.deletedAt = LocalDateTime.now();
         this.deletedBy = deleter;
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
     }
 }
