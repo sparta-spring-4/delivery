16147985f774ede439629c210e9d707885916c6d
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-10-30 15:58:37 +0900

    [Refactor] Rename auditing configuration classes and update package structure

--- src/main/java/com/zts/delivery/global/persistence/config/JpaConfig.java
+++ src/main/java/com/zts/delivery/global/persistence/config/JpaConfig.java
@@ -1,5 +1,8 @@
-package com.zts.delivery.config;
+package com.zts.delivery.global.persistence.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -7,7 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-public class JpaAuditingConfig {
+public class JpaConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
 
     @Bean
     public AuditorAware<String> auditorProvider(){
