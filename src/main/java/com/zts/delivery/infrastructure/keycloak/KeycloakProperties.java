5f5bcd54c1c6a34938986c005dbfecb7c43a944c
JKjiwon <jwkim.oa@gmail.com>
2025-11-01 14:49:53 +0900

    [Feat] Keycloak 서버 설정 & 토큰 발급
    # Conflicts:
    #	build.gradle
    #	src/main/java/com/zts/delivery/DeliveryApplication.java
    #	src/main/resources/application-local.yml

---
+++ src/main/java/com/zts/delivery/infrastructure/keycloak/KeycloakProperties.java
@@ -0,0 +1,15 @@
+package com.zts.delivery.infrastructure.keycloak;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "keycloak")
+public class KeycloakProperties {
+    private String serverUrl;
+    private String realm;
+    private String clientId;
+    private String clientSecret;
+    private String adminUsername;
+    private String adminPassword;
+}
