0751a21e420aaae247a655e9fbbfc191f77f0593
JKjiwon <jwkim.oa@gmail.com>
2025-11-03 07:10:04 +0900

    [Refactor] Keycloak 로직 Repository로 추상화

--- src/main/java/com/zts/delivery/user/application/dto/UserProfile.java
+++ src/main/java/com/zts/delivery/user/application/dto/UserProfile.java
@@ -1,5 +1,6 @@
 package com.zts.delivery.user.application.dto;
 
+import com.zts.delivery.user.domain.User;
 import lombok.Builder;
 
 import java.util.UUID;
@@ -12,4 +13,13 @@ public record UserProfile(
         String name,
         String phone
 ) {
+    public static UserProfile of(User user) {
+        return UserProfile.builder()
+                .userId(user.getId().getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .name(user.getLastName() + " " + user.getFirstName())
+                .phone(user.getPhone())
+                .build();
+    }
 }
