4fd6c135c35f579cd7f147f3de77ae8aa9e5eef7
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-03 11:07:49 +0900

    [Feat] 카트 도메인 구현
    - Cart : 장바구니를 구현한 클래스
    - CartItem : 장바구니 내 품목

---
+++ src/main/java/com/zts/delivery/order/domain/cart/CartItem.java
@@ -0,0 +1,37 @@
+package com.zts.delivery.order.domain.cart;
+
+import com.zts.delivery.global.persistence.Price;
+import com.zts.delivery.global.persistence.converter.PriceConverter;
+import com.zts.delivery.item.domain.ItemId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem {
+    @Embedded
+    @Column(name="item_id")
+    private ItemId id;
+
+    @Column(length=60)
+    private String itemName;
+
+    @Convert(converter = PriceConverter.class)
+    private Price itemPrice;
+
+    @Builder
+    public CartItem(ItemId id, String itemName,Price itemPrice) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+}
