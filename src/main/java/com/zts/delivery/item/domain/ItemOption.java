28a583ed0540aa031ed4698532cc8a9508c8874c
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-03 10:51:27 +0900

    [Feat] 아이템 관련 클래스 추가 구현
    - Item : storeId 추가, status 추가, stock 추가, Transient 필드 outOfStock 추가
    - Item Method : Builder, getPriceValue, setStock, addOption, removeOption,removeOptionAll, getTotal 추가
    - ItemId : EmbeddableId 수행가능하게 변경
    - ItemOption : 가격 추가
    - ItemStatus : Item에 상태를 추가하기 위한 Enum 구현

--- src/main/java/com/zts/delivery/item/domain/ItemOption.java
+++ src/main/java/com/zts/delivery/item/domain/ItemOption.java
@@ -1,8 +1,10 @@
-package com.zts.delivery.menu.domain;
+package com.zts.delivery.item.domain;
 
+import com.zts.delivery.global.persistence.Price;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -16,4 +18,12 @@ public class ItemOption {
     @Column(nullable = false, length=60)
     private String optionName;
 
+    @Column(nullable = false)
+    private Price price;
+
+    @Builder
+    public ItemOption(String optionName, Price price) {
+        this.optionName = optionName;
+        this.price = price;
+    }
 }
