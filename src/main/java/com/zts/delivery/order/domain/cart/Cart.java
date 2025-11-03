3b829c8827164e14c7d07c3a4723056f60fbbbc8
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-03 11:07:49 +0900

    [Feat] 카트 도메인 구현
    - Cart : 장바구니를 구현한 클래스
    - CartItem : 장바구니 내 품목

---
+++ src/main/java/com/zts/delivery/order/domain/cart/Cart.java
@@ -0,0 +1,70 @@
+package com.zts.delivery.order.domain.cart;
+
+import com.zts.delivery.global.persistence.Price;
+import com.zts.delivery.global.persistence.converter.PriceConverter;
+import com.zts.delivery.user.UserId;
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@ToString
+@Getter
+@Entity
+@Access(AccessType.FIELD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Cart {
+    @EmbeddedId
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private CartId id;
+
+    @Embedded
+    private UserId userId;
+
+    @Embedded
+    private CartItem item;
+
+    private int quantity;
+
+    @Convert(converter = PriceConverter.class)
+    private Price totalPrice;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Cart(UserId userId, CartItem item, int quantity) {
+        this.userId = userId;
+        this.quantity = quantity;
+        setCartItem(item);
+        calculateTotalPrice();
+    }
+
+    // 장바구니 상품이 없다면 담을 수 없음
+    private void setCartItem(CartItem item) {
+        // if (item == null) throw new CartItemNotFoundException();
+
+        this.item = item;
+    }
+
+    // 장바구니 상품별 총 합계
+    private void calculateTotalPrice() {
+        if (item == null) return;
+
+        totalPrice = new Price(item.getItemPrice().getValue() * quantity);
+    }
+}
