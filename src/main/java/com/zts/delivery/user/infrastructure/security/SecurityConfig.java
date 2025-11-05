ee86539d687302dc4fbf3255d8c49a8cf0c44197
JKjiwon <jwkim.oa@gmail.com>
2025-11-05 14:15:23 +0900

    [Feat] 시큐리티 특정 도메인 CORS 허용 설정

--- src/main/java/com/zts/delivery/user/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/zts/delivery/user/infrastructure/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.zts.delivery.user.infrastructure.security;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,21 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    private static final RequestMatcher[] PUBLIC_MATCHERS = new RequestMatcher[]{
+            PathPatternRequestMatcher.withDefaults().matcher("/v1/users/token/**"),
+            PathPatternRequestMatcher.withDefaults().matcher("/v1/users/signup/**")
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,8 +35,9 @@ public class SecurityConfig {
                 = new JwtToUserAuthenticationConverter(new JwtToUserRoleConverter());
 
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/v1/users/token/**", "/v1/users/signup/**").permitAll()
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(AbstractHttpConfigurer::disable)
