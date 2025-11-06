84de5ea7fb907f2cea81f157a805913b7bc1252a
JKjiwon <jwkim.oa@gmail.com>
2025-11-06 17:19:12 +0900

    [Feat] role 변경 추가

--- src/main/java/com/zts/delivery/user/domain/UserRole.java
+++ src/main/java/com/zts/delivery/user/domain/UserRole.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserRole {
     CUSTOMER("고객"),
-    OWNER("Store 주인"),
+    STAFF("Store 직원"),
+    OWNER("Store 사장"),
     MANAGER("관리자"),
     MASTER("마스터(모든 권한을 가진다.)");
 
