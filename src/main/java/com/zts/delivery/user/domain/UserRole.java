ec390861e0770b3a61485069bd3d70c905f5c9ad
JKjiwon <jwkim.oa@gmail.com>
2025-11-06 16:31:35 +0900

    [Feat] role 추가, role 계층 설절

--- src/main/java/com/zts/delivery/user/domain/UserRole.java
+++ src/main/java/com/zts/delivery/user/domain/UserRole.java
@@ -1,5 +1,13 @@
 package com.zts.delivery.user.domain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum UserRole {
-    USER, ADMIN
+    CUSTOMER("고객"),
+    OWNER("Store 주인"),
+    MANAGER("관리자"),
+    MASTER("마스터(모든 권한을 가진다.)");
+
+    private final String description;
 }
