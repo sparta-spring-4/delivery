d7a59eff4025fb3237cd66dbafdac382a78d4c7e
JKjiwon <jwkim.oa@gmail.com>
2025-11-09 00:30:41 +0900

    [Chore] 회원, 결제 컨트롤러 스웨거 추가

--- src/main/java/com/zts/delivery/payment/presentation/controller/TossPaymentController.java
+++ src/main/java/com/zts/delivery/payment/presentation/controller/TossPaymentController.java
@@ -9,6 +9,8 @@ import com.zts.delivery.payment.application.service.TossConfirmService;
 import com.zts.delivery.payment.presentation.dto.CancelTossPaymentRequest;
 import com.zts.delivery.payment.presentation.dto.TossPaymentConfirmRequest;
 import com.zts.delivery.user.infrastructure.security.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Toss payment 승인/취소 API", description = "Toss payment 승인 / 취소 기능을 제공합니다.")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +30,7 @@ public class TossPaymentController {
 
     private final TossCancelService tossCancelService;
 
+    @Operation(summary = "결제 승인", description = "결제가 인증된 후 결제를 승인합니다.")
     @PostMapping("/confirm")
     public void confirm(@AuthenticationPrincipal UserPrincipal user,
                         @RequestBody TossPaymentConfirmRequest req) {
@@ -39,6 +43,7 @@ public class TossPaymentController {
         tossConfirmService.confirm(user.userId(), confirmTossPayment);
     }
 
+    @Operation(summary = "결제 취소", description = "결제가 완료된 후 결제를 취소 할 수 있습니다.")
     @PostMapping("/cancel")
     public void cancel(@RequestBody CancelTossPaymentRequest req) {
         CancelTossPayment cancelTossPayment = CancelTossPayment.builder()
